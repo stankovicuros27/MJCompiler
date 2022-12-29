5fad9888b1870d4b90536a51b0747ce617275d41
Uros Stankovic <stankovicuros27@Uross-MacBook-Pro.local>
2022-12-29 18:43:36 +0100

    Project setup, Lexical analysis exercise

---
+++ src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,34 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+	
+	// In later stages, this Class will be auto generated
+	
+	// Keywords
+	public static final int PROG = 1;
+	public static final int PRINT = 2;
+	public static final int RETURN = 3;
+	
+	// Identifiers
+	public static final int IDENT = 4;
+	
+	// Constants
+	public static final int NUMBER = 5;
+	
+	// Operators
+	public static final int PLUS = 6;
+	public static final int EQUAL = 7;
+	public static final int COMMA = 8;
+	public static final int SEMI = 9;
+	public static final int LPAREN = 10;
+	public static final int RPAREN = 11;
+	public static final int LBRACE = 12;
+	public static final int RBRACE = 13;
+	
+	// EOF
+	public static final int EOF = 14;
+	
+	// RETURN TYPES
+	public static final int VOID = 15;
+	
+}
